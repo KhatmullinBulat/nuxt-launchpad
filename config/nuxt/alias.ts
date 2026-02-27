f93e74a608186808d5896902c2cafacc8d81b743
b.khatmullin <KhatmullinBR@yandex.ru>
2026-02-27 17:57:07 +0500

    fix shared/assets imports

--- config/nuxt/alias.ts
+++ config/nuxt/alias.ts
@@ -2,13 +2,11 @@ import type { NuxtConfig } from 'nuxt/schema'
 import { resolve } from 'pathe'
 
 export const alias: NuxtConfig['alias'] = {
+  '~': resolve(__dirname, '../../app'),
+  '@': resolve(__dirname, '../../app'),
+
   '#entities': resolve(__dirname, '../../app/entities'),
   '#features': resolve(__dirname, '../../app/features'),
   '#widgets': resolve(__dirname, '../../app/widgets'),
   '#shared': resolve(__dirname, '../../app/shared'),
-
-  '@/assets': resolve(__dirname, '../../app/shared/assets'),
-  '~/assets': resolve(__dirname, '../../app/shared/assets'),
-  '@/composables': resolve(__dirname, '../../app/shared/composables'),
-  '~/composables': resolve(__dirname, '../../app/shared/composables'),
 }
